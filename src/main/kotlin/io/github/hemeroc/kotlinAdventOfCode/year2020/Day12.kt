fd7d81339802e445c8a673f768908cbcc482646b
Dominik Moser <dominik.moser@erstegroup.com>
2020-12-12 17:29:49 +0100

    day12

--- src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2020/Day12.kt
+++ src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2020/Day12.kt
@@ -11,28 +11,60 @@ fun main() {
         val programInput =
             readLines(2020, "input12.txt")
                 .map { InstructionHolder(it[0], it.substring(1).toInt()) }
-        val start = ShipPosition(Position(0, 0), E)
-        val end = programInput.fold(start) { ship, (instruction, value) ->
-            when (instruction) {
-                'N', 'E', 'S', 'W' -> ship.copy(position = ship.position + Direction.valueOf("$instruction").position * value)
-                'F' -> ship.copy(position = ship.position + ship.heading.position * value)
-                'L' -> ship.copy(heading = Direction.values()[((ship.heading.ordinal - (value / 90)) % 4)
-                    .let { if (it >= 0) it else 4 - abs(it) }]
-                )
-                'R' -> ship.copy(heading = Direction.values()[(ship.heading.ordinal + (value / 90)) % 4])
-                else -> ship
-            }.also { println("$instruction$value -> $it") }
-        }
+        val ship1 = solve1(programInput, Position(0, 0))
+        val ship2 = solve2(programInput, Position(0, 0))
         println(
             """
-                Manhattan distance part1: ${abs(end.position.x) + abs(end.position.y)}
-                Occupied seats part2: 2
+                Manhattan distance part1: ${abs(ship1.x) + abs(ship1.y)}
+                Manhattan distance part2: ${abs(ship2.x) + abs(ship2.y)}
             """.trimIndent()
         )
     }.also { println("Calculated in ${it}ms") }
 }
 
-data class ShipPosition(val position: Position, val heading: Direction)
+private fun solve1(
+    programInput: List<InstructionHolder>,
+    initialShipPosition: Position
+): Position {
+    var shipHeading = E
+    return programInput.fold(initialShipPosition) { shipPosition, (instruction, value) ->
+        when (instruction) {
+            'N', 'E', 'S', 'W' -> shipPosition + Direction.valueOf("$instruction").position * value
+            'F' -> shipPosition + shipHeading.position * value
+            'L' -> {
+                shipHeading = Direction.values()[((shipHeading.ordinal - (value / 90)) % 4)
+                    .let { if (it >= 0) it else 4 - abs(it) }]
+                shipPosition
+            }
+            'R' -> {
+                shipHeading = Direction.values()[(shipHeading.ordinal + (value / 90)) % 4]
+                shipPosition
+            }
+            else -> shipPosition
+        }
+    }
+}
+
+private fun solve2(
+    programInput: List<InstructionHolder>,
+    initialShipPosition: Position
+): Position {
+    var shipPosition = initialShipPosition
+    programInput.fold(Position(10, 1)) { waypoint, (instruction, value) ->
+        when (instruction) {
+            'N', 'E', 'S', 'W' -> waypoint + Direction.valueOf("$instruction").position * value
+            'F' -> {
+                shipPosition += waypoint * value
+                waypoint
+            }
+            'L' -> (0 until (value / 90)).fold(waypoint) { oldPos, _ -> Position(x = oldPos.y * -1, y = oldPos.x) }
+            'R' -> (0 until (value / 90)).fold(waypoint) { oldPos, _ -> Position(x = oldPos.y, y = oldPos.x * -1) }
+            else -> waypoint
+        }
+    }
+    return shipPosition
+}
+
 enum class Direction(val position: Position) {
     N(Position(0, 1)),
     E(Position(1, 0)),
