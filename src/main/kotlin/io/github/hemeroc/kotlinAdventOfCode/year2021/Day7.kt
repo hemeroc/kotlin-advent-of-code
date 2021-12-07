3238ce9fa67fa000ddc41bef57ec6b123f31f40d
Dominik Moser <severus.drakone@gmail.com>
2021-12-07 07:37:20 +0100

    Day7 - performance

--- src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2021/Day7.kt
+++ src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2021/Day7.kt
@@ -6,11 +6,13 @@ import kotlin.system.measureTimeMillis
 
 fun main() {
     measureTimeMillis {
-        with(readLines(2021, "input7.txt").single().split(",").map { it.toInt() }) {
-            (minOf { it }..maxOf { it })
-                .map { to -> sumOf { from -> abs(from - to) } to sumOf { from -> (1..abs(from - to)).sum() } }
+        with(readLines(2021, "input7.txt").single().split(",").map { it.toInt() }.sorted()) {
+            (first()..last())
+                .map { to -> sumOf { from -> abs(from - to) } to sumOf { from -> partialSum(abs(from - to)) } }
                 .let { consumption -> consumption.minOf { it.first } to consumption.minOf { it.second } }
                 .also { println("Solution 1: ${it.first}\nSolution 2: ${it.second}") }
         }
     }.also { println("Calculated in ${it}ms") }
 }
+
+fun partialSum(n: Int) = n * (n + 1) / 2
