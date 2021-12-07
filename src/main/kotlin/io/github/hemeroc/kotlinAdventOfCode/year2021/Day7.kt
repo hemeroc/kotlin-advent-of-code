f5bfc4d2892481253f92ef193f7f1f960563b81c
Dominik Moser <severus.drakone@gmail.com>
2021-12-07 07:25:15 +0100

    Day7

--- src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2021/Day7.kt
+++ src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2021/Day7.kt
@@ -6,45 +6,11 @@ import kotlin.system.measureTimeMillis
 
 fun main() {
     measureTimeMillis {
-        val data = readLines(2021, "input7.txt")
-            .flatMap { line -> line.split(",").map { value -> value.toInt() } }
-            .sorted()
-        val start = data.minOf { it }
-        val end = data.maxOf { it }
-        val offset = start
-        val fuelConsumptionsSolution1 = IntArray(end - start) { 0 }
-        val fuelConsumptionsSolution2 = IntArray(end - start) { 0 }
-        fuelConsumptionsSolution1.indices.forEach { index ->
-            val movePosition = index + offset
-            data.forEach {  currentPosition ->
-                fuelConsumptionsSolution1[index] += abs(currentPosition - movePosition)
-                fuelConsumptionsSolution2[index] += (abs(currentPosition - movePosition) downTo 1).sum()
-            }
+        with(readLines(2021, "input7.txt").single().split(",").map { it.toInt() }) {
+            (minOf { it }..maxOf { it })
+                .map { to -> sumOf { from -> abs(from - to) } to sumOf { from -> (1..abs(from - to)).sum() } }
+                .let { consumption -> consumption.minOf { it.first } to consumption.minOf { it.second } }
+                .also { println("Solution 1: ${it.first}\nSolution 2: ${it.second}") }
         }
-        println(fuelConsumptionsSolution1.minOf { it })
-        println(fuelConsumptionsSolution2.minOf { it })
     }.also { println("Calculated in ${it}ms") }
 }
-
-
-
-// HEAP SPACE ERROR
-//fun main() {
-//    measureTimeMillis {
-//        val data = readLines(2021, "input6.txt")
-//            .flatMap { line -> line.split(",").map { value -> value.toInt() } }
-//            .sorted()
-//        val start = data.minOf { it }
-//        val end = data.maxOf { it }
-//        val offset = start
-//        val fuelConsumptions = IntArray(end - start) { 0 }
-//        fuelConsumptions.forEach { index ->
-//            val movePosition = index + offset
-//            data.forEach {  currentPosition ->
-//                fuelConsumptions[index] += abs(currentPosition - movePosition)
-//            }
-//            println(fuelConsumptions)
-//        }
-//        println(fuelConsumptions.maxOf { it })
-//    }.also { println("Calculated in ${it}ms") }
-//}
