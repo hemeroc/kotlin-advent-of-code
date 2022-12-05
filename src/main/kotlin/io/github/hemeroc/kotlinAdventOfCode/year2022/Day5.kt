8fa79252c1ec6130815cf3b16327d4548b719536
Dominik Moser <severus.drakone@gmail.com>
2022-12-06 00:39:58 +0100

    day 5

--- src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2022/Day5.kt
+++ src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2022/Day5.kt
@@ -4,36 +4,38 @@ import io.github.hemeroc.kotlinAdventOfCode.util.readLines
 import kotlin.system.measureTimeMillis
 
 fun main() {
+    val data = readLines(2022, "input5.txt")
     val instructions =
-        readLines(2022, "input5.txt").drop(10).map { it.drop(5).split(" from ", " to ").map { it.toInt() } }
+        data.dropWhile { !it.startsWith("move") }
+            .map { instructionLine ->
+                instructionLine
+                    .split(" ")
+                    .let { Triple(it[1].toInt(), it[3].toInt() - 1, it[5].toInt() - 1) }
+            }
     measureTimeMillis {
-        with(towers()) {
-            instructions.forEach { (times, from, to) ->
-                get(to - 1).addAll(get(from - 1).removeLast(times))
-            }
-            println(map { it.last() }.joinToString(""))
-        }
-        with(towers()) {
-            instructions.forEach { (times, from, to) ->
-                get(to - 1).addAll(get(from - 1).removeLast(times, true))
-            }
-            println(map { it.last() }.joinToString(""))
-        }
+        println(towers(data).applyInstructions(instructions, true).map { it.last() }.joinToString(""))
+        println(towers(data).applyInstructions(instructions, false).map { it.last() }.joinToString(""))
     }.also { println("Calculated in ${it}ms") }
 }
 
-private fun towers(): List<ArrayDeque<Char>> =
-    listOf(
-        ArrayDeque("HRBDZFLS".toList()),
-        ArrayDeque("TBMZR".toList()),
-        ArrayDeque("ZLCHNS".toList()),
-        ArrayDeque("SCFJ".toList()),
-        ArrayDeque("PGHWRZB".toList()),
-        ArrayDeque("VJZGDNMT".toList()),
-        ArrayDeque("GLNWFSPQ".toList()),
-        ArrayDeque("MZR".toList()),
-        ArrayDeque("MCLGVRT".toList()),
-    )
+private fun List<ArrayDeque<Char>>.applyInstructions(instructions: List<Triple<Int, Int, Int>>, reversed: Boolean) =
+    instructions.forEach { (times, from, to) ->
+        get(to).addAll(get(from).removeLast(times, reversed))
+    }.let { this }
+
+private fun towers(data: List<String>): List<ArrayDeque<Char>> {
+    val stackData = data.takeWhile { it.isNotEmpty() }
+    val stackCount = stackData.last().split(" ").last().toInt()
+    return (0 until stackCount).map { index ->
+        val position = 1 + (index * 4)
+        ArrayDeque(
+            stackData
+                .mapNotNull { it.getOrNull(position) }
+                .filter { it in ('A'..'Z') }
+                .toList()
+        )
+    }
+}
 
 private fun <E> ArrayDeque<E>.removeLast(times: Int, reversed: Boolean = false): Collection<E> =
     (1..times).map { removeLast() }.let {
