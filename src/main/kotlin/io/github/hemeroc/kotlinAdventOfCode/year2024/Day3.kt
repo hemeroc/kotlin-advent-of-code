cfb7cd7ae0a869c275462c81e1df9d06fc1e5c3e
Dominik Moser <severus.drakone@gmail.com>
2024-12-03 07:49:22 +0100

    day3

---
+++ src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2024/Day3.kt
@@ -0,0 +1,41 @@
+package io.github.hemeroc.kotlinAdventOfCode.year2024
+
+import io.github.hemeroc.kotlinAdventOfCode.util.readLines
+import kotlin.system.measureNanoTime
+
+private const val RIGHT = "right"
+private const val LEFT = "left"
+private const val DO = "do"
+private const val DONT = "don"
+
+fun main() {
+    val regex = Regex("mul\\((?<$LEFT>\\d{1,3}),(?<$RIGHT>\\d{1,3})\\)|(?<$DO>do)\\(\\)|(?<$DONT>don)'t\\(\\)")
+    val data = readLines(2024, "input3.txt")
+        .flatMap { regex.findAll(it) }.toList()
+    measureNanoTime {
+        data.sumOf {
+            if (it.groups[LEFT] != null) {
+                it.groups[LEFT]!!.value.toLong() * it.groups[RIGHT]!!.value.toLong()
+            } else {
+                0L
+            }
+        }.also { println("Part 1: $it ") }
+    }.also { println("Calculated in ${it / 1000}µs") }
+    measureNanoTime {
+        var enabled = true
+        data.sumOf {
+            when {
+                it.groups[DO] != null -> {
+                    enabled = true; 0L
+                }
+
+                it.groups[DONT] != null -> {
+                    enabled = false; 0L
+                }
+
+                enabled -> it.groups[LEFT]!!.value.toLong() * it.groups[RIGHT]!!.value.toLong()
+                else -> 0L
+            }
+        }.also { println("Part 2: $it ") }
+    }.also { println("Calculated in ${it / 1000}µs") }
+}
