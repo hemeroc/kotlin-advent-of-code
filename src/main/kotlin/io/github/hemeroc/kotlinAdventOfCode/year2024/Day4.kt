2e6f45494948c50e682fa507d6b4726fa381aa47
Dominik Moser <severus.drakone@gmail.com>
2024-12-04 11:01:45 +0100

    day4 nicer

--- src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2024/Day4.kt
+++ src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2024/Day4.kt
@@ -1,54 +1,76 @@
 package io.github.hemeroc.kotlinAdventOfCode.year2024
 
 import io.github.hemeroc.kotlinAdventOfCode.util.readLines
+import io.github.hemeroc.kotlinAdventOfCode.year2019.tail
+import io.github.hemeroc.kotlinAdventOfCode.year2024.Direction.DOWN_LEFT
+import io.github.hemeroc.kotlinAdventOfCode.year2024.Direction.DOWN_RIGHT
+import io.github.hemeroc.kotlinAdventOfCode.year2024.Direction.UP_LEFT
+import io.github.hemeroc.kotlinAdventOfCode.year2024.Direction.UP_RIGHT
+import kotlin.system.measureNanoTime
 
-fun main() {
-    val data = readLines(2024, "input4.txt").map { it.toCharArray() }.toTypedArray()
-    var sumXmas = 0
-    var sumRealXmas = 0
-    for (x in data.indices) {
-        for (y in data[x].indices) {
-            if (data[x][y] == 'X') {
-                sumXmas += search(data, x, y, 1, 0, 'M') + // down
-                        search(data, x, y, 0, 1, 'M') + // right
-                        search(data, x, y, 1, 1, 'M') + // down-right
-                        search(data, x, y, -1, 0, 'M') + // up
-                        search(data, x, y, 0, -1, 'M') + // left
-                        search(data, x, y, -1, -1, 'M') + // up-left
-                        search(data, x, y, -1, 1, 'M') + // up-right
-                        search(data, x, y, 1, -1, 'M') + // down-left
-                        0
-            }
-            if (data[x][y] == 'A') {
-                val foundXMas = search(data, x - 2, y - 2, 1, 1, 'M') +
-                        search(data, x + 2, y + 2, -1, -1, 'M') +
-                        search(data, x + 2, y - 2, -1, 1, 'M') +
-                        search(data, x - 2, y + 2, 1, -1, 'M') +
-                        0
-                if (foundXMas >= 2) {
-                    sumRealXmas++
-                }
+private const val SEARCHTERM = "MAS"
+private val DIAGONALS = setOf(DOWN_RIGHT, UP_LEFT, UP_RIGHT, DOWN_LEFT)
 
-            }
-        }
-    }
-    println(sumXmas)
-    println(sumRealXmas)
+data class Point(val x: Int, val y: Int) {
+    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+    operator fun times(multiply: Int) = Point(x * multiply, y * multiply)
 }
 
-tailrec fun search(data: Array<CharArray>, x: Int, y: Int, dirX: Int, dirY: Int, search: Char): Int {
-    val newX = x + dirX
-    val newY = y + dirY
-    if (newX < 0 || newY < 0 || newX >= data.size || newY >= data[newX].size) {
+enum class Direction(private val offset: Point) {
+    DOWN(Point(1, 0)),
+    RIGHT(Point(0, 1)),
+    DOWN_RIGHT(Point(1, 1)),
+    UP(Point(-1, 0)),
+    LEFT(Point(0, -1)),
+    UP_LEFT(Point(-1, -1)),
+    UP_RIGHT(Point(-1, 1)),
+    DOWN_LEFT(Point(1, -1)),
+    ;
+
+    operator fun plus(point: Point) = offset + point
+    operator fun times(multiply: Int) = offset * multiply
+}
+
+private operator fun List<CharArray>.get(point: Point) =
+    if (point.x !in indices || point.y !in this[point.x].indices) {
+        null
+    } else {
+        this[point.x][point.y]
+    }
+
+fun main() {
+    val data = readLines(2024, "input4.txt").map { it.toCharArray() }
+    measureNanoTime {
+        data.indices.sumOf { x ->
+            data[x].indices.sumOf { y ->
+                Point(x, y).let { point ->
+                    if (data[point] != 'X') {
+                        0
+                    } else {
+                        Direction.entries.sumOf { search(data, point, it, SEARCHTERM) }
+                    }
+                }
+            }
+        }.also { print("Part 1: $it ") }
+    }.also { println("Calculated in ${it / 1000}µs") }
+    measureNanoTime {
+        data.indices.sumOf { x ->
+            data[x].indices.count { y ->
+                Point(x, y).let { point ->
+                    data[point] == 'A' && DIAGONALS.sumOf { search(data, point + it * -2, it, SEARCHTERM) } >= 2
+                }
+            }
+        }.also { print("Part 2: $it ") }
+    }.also { println("Calculated in ${it / 1000}µs") }
+}
+
+tailrec fun search(data: List<CharArray>, point: Point, direction: Direction, searchterm: String): Int {
+    if (searchterm.isEmpty()) {
+        return 1
+    }
+    val newPosition = direction + point
+    if (data[newPosition] != searchterm.first()) {
         return 0
     }
-    if (search != data[newX][newY]) {
-        return 0
-    }
-    return when (search) {
-        'M' -> search(data, newX, newY, dirX, dirY, 'A')
-        'A' -> search(data, newX, newY, dirX, dirY, 'S')
-        'S' -> 1
-        else -> 0
-    }
+    return search(data, newPosition, direction, searchterm.tail())
 }
