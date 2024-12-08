61ce61b936db991b9f4f75ca7e9b332d0e880746
Dominik Moser <severus.drakone@gmail.com>
2024-12-08 16:25:08 +0100

    day8

--- src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2024/day8/Day8.kt
+++ src/main/kotlin/io/github/hemeroc/kotlinAdventOfCode/year2024/day8/Day8.kt
@@ -1,6 +1,7 @@
 package io.github.hemeroc.kotlinAdventOfCode.year2024.day8
 
 import io.github.hemeroc.kotlinAdventOfCode.util.readLines
+import kotlin.time.measureTime
 
 private data class Point(val x: Int, val y: Int) {
     operator fun plus(other: Point) = Point(x + other.x, y + other.y)
@@ -12,32 +13,43 @@ private data class Point(val x: Int, val y: Int) {
 fun main() {
 
     val data = readLines(2024, "input8.txt").map { it.toCharArray() }
-    val charMap = mutableMapOf<Char, MutableList<Point>>()
-
-    data.forEachIndexed { x, row ->
-        row.forEachIndexed { y, value ->
-            charMap.getOrPut(value) { mutableListOf() }.add(Point(x, y))
+    val antennas = buildMap<Char, MutableList<Point>> {
+        data.forEachIndexed { x, row ->
+            row.forEachIndexed { y, value ->
+                if (value != '.') {
+                    getOrPut(value) { mutableListOf() }.add(Point(x, y))
+                }
+            }
         }
     }
-
-    println(solve1(data, charMap)) // 357
-    println(solve2(data, charMap)) // 1266
+    measureTime {
+        solve1(data, antennas).also {
+            print("Part 1: $it ")
+        }
+    }.also { println("Calculated in $it") }
+    measureTime {
+        solve2(data, antennas).also {
+            print("Part 2: $it ")
+        }
+    }.also { println("Calculated in $it") }
 }
 
-private fun solve1(map: List<CharArray>, antennas: MutableMap<Char, MutableList<Point>>): Int {
+private fun solve1(map: List<CharArray>, antennas: Map<Char, List<Point>>): Int {
     val antiNodes = mutableSetOf<Point>()
-    for ((char, points) in antennas.entries) {
-        if (char == '.') continue
-        for (i in points.indices) {
-            for (j in i + 1 until points.size) {
-                val diff = points[j] - points[i]
-                val candidate1 = points[i] - diff
-                val candidate2 = points[j] + diff
-                if (candidate1 isOn map) {
-                    antiNodes.add(candidate1)
-                }
-                if (candidate2 isOn map) {
-                    antiNodes.add(candidate2)
+    for (locations in antennas.values) {
+        for (i in locations.indices) {
+            for (j in i + 1 until locations.size) {
+                (locations[j] - locations[i]).let { distance ->
+                    (locations[i] - distance).let {
+                        if (it isOn map) {
+                            antiNodes.add(it)
+                        }
+                    }
+                    (locations[j] + distance).let {
+                        if (it isOn map) {
+                            antiNodes.add(it)
+                        }
+                    }
                 }
             }
         }
@@ -45,23 +57,24 @@ private fun solve1(map: List<CharArray>, antennas: MutableMap<Char, MutableList<
     return antiNodes.size
 }
 
-private fun solve2(map: List<CharArray>, antennas: MutableMap<Char, MutableList<Point>>): Int {
+private fun solve2(map: List<CharArray>, antennas: Map<Char, List<Point>>): Int {
     val antiNodes = mutableSetOf<Point>()
-    for ((char, points) in antennas.entries) {
-        if (char == '.') continue
-        for (i in points.indices) {
-            for (j in i + 1 until points.size) {
-                val diff = points[j] - points[i]
-                var candidate1 = points[i]
-                while (candidate1 isOn map) {
-                    antiNodes.add(candidate1)
-                    candidate1 -= diff
+    for (locations in antennas.values) {
+        for (i in locations.indices) {
+            for (j in i + 1 until locations.size) {
+                (locations[j] - locations[i]).let { distance ->
+                    var candidate1 = locations[i]
+                    while (candidate1 isOn map) {
+                        antiNodes.add(candidate1)
+                        candidate1 -= distance
+                    }
+                    var candidate2 = locations[j]
+                    while (candidate2 isOn map) {
+                        antiNodes.add(candidate2)
+                        candidate2 += distance
+                    }
                 }
-                var candidate2 = points[j]
-                while (candidate2 isOn map) {
-                    antiNodes.add(candidate2)
-                    candidate2 += diff
-                }
+
             }
         }
     }
